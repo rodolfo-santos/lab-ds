ec3175cde6d50f4d1c942f9c1746c0becb74e5ef
Rodolfo Santos <rodolfodossantos29@gmail.com>
2022-10-12 19:44:39 -0300

    🔨 disable arg type

--- src/components/Text.stories.tsx
+++ src/components/Text.stories.tsx
@@ -42,6 +42,11 @@ export const CustomComponent: StoryObj<TextProps> = {
       table: {
         disable: true
       }
+    },
+    asChild: {
+      table: {
+        disable: true
+      }
     }
   }
 };
