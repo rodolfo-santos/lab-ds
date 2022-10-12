425f263baa4a2f043c1783f880574da0c2c05172
Rodolfo Santos <rodolfodossantos29@gmail.com>
2022-10-12 19:11:00 -0300

    ✨ adding props to text component

--- src/components/Text.stories.tsx
+++ src/components/Text.stories.tsx
@@ -1,8 +1,25 @@
-import { Text } from './Text';
+import { Meta, StoryObj } from '@storybook/react';
+import { Text, TextProps } from './Text';
 
 export default {
   title: 'Components/Text',
-  component: Text
+  component: Text,
+  args: {
+    children: 'Lorem ipsum',
+    size: 'md'
+  }
+} as Meta<TextProps>;
+
+export const Default: StoryObj<TextProps> = {};
+
+export const Small: StoryObj<TextProps> = {
+  args: {
+    size: 'sm'
+  }
 };
 
-export const Default = {};
+export const Large: StoryObj<TextProps> = {
+  args: {
+    size: 'lg'
+  }
+};
--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,3 +1,20 @@
-export function Text() {
-  return <h1>Hello World</h1>;
+import { clsx } from 'clsx';
+
+export interface TextProps {
+  size?: 'sm' | 'md' | 'lg';
+  children: string;
+}
+
+export function Text({ size = 'md', children }: TextProps) {
+  return (
+    <span
+      className={clsx('text-gray-100 font-sans', {
+        'text-xs': size === 'sm',
+        'text-sm': size === 'md',
+        'text-md': size === 'lg'
+      })}
+    >
+      {children}
+    </span>
+  );
 }
